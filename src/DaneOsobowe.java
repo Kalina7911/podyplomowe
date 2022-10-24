56bdeb9360a69084df47a9012edf607b8f881505
a7911 k <alina7911@gmail.com>
2022-10-24 16:02:29 +0200

    Initial commit

---
+++ src/DaneOsobowe.java
@@ -0,0 +1,21 @@
+public class DaneOsobowe {
+
+    public static void main(String[] args) {
+
+
+        String name = "Alina";
+        long idNumber = 40091209404l;
+        char gender = 'f';
+        String surname = "Kowalska";
+        int age = 102;
+
+
+        System.out.println("Imię: " + name);
+        System.out.println("Nazwisko: " + surname);
+        System.out.println("ID: " + idNumber);
+        System.out.println("Płeć: " + gender);
+        System.out.println("Wiek: " + age);
+
+    }
+
+}
