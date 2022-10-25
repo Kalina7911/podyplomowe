e35e6991147c174d6e33a0215e9303024d1b135d
kalina7911 <kotnarow@student. wszib.edu.pl>
2022-10-25 16:07:58 +0200

    Nowy plik:Classes o zajęciach

---
+++ src/Classes.java
@@ -0,0 +1,17 @@
+public class Classes {
+
+    public static void main(String[] args) {
+
+        String class1 = "Zarządzanie projektem";
+        String class2 = "Projektowanie aplikacji webowych";
+        String class3 = "Programowanie w języku Java";
+
+        int hours1 = 16;
+        int hours2 = 40;
+        int hours3 = 60;
+
+        System.out.println("Na zajęcia \"" + class1 + "\" jest przeznaczonych " + hours1 + " godzin lekcyjnych.");
+        System.out.println("Na zajęcia \"" + class2 + "\" jest przeznaczonych " + hours2 + " godzin lekcyjnych.");
+        System.out.println("Na zajęcia \"" + class3 + "\" jest przeznaczonych " + hours3 + " godzin lekcyjnych.");
+    }
+}
